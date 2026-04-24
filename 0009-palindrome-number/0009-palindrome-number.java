04d1069c2664fb84b032d4c214950ef1d41c1edb
Nguyen Minh Duong <2511130090@st.hcmuaf.edu.vn>
2026-04-24 18:56:19 +0700

    Time: 6 ms (23.28%), Space: 45.7 MB (94.26%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,6 +3,9 @@ class Solution {
         String a = String.valueOf(x);
         int i= 0;
         int j= a.length()-1;
+        if(x<0){
+            return false;
+        }
         while(i<=(a.length())/2&&i<j){
             if(a.charAt(i)!=a.charAt(j)){
                 return false;
