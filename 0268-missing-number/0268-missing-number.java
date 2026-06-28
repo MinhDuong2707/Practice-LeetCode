da3dad7bf2474713d0f0f02c1fe332a7f25bbf84
Nguyen Minh Duong <2511130090@st.hcmuaf.edu.vn>
2026-06-28 09:02:24 +0700

    Time: 6 ms (17.79%), Space: 47.3 MB (65.44%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -4,15 +4,13 @@ class Solution {
         for(int n:nums){
             set.add(n);
         }
-        int result=0;
         int i=0;
         while(i<=nums.length){
             if(!set.contains(i)){
-                result=i;
                 break;
             }
             i++;
         }
-        return result;
+        return i;
     }
 }
