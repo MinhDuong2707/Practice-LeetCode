8b3bd788b8d81e8b7cb768286943b60cddece260
Nguyen Minh Duong <2511130090@st.hcmuaf.edu.vn>
2026-08-31 08:48:59 +0700

    Time: 9 ms (28.44%), Space: 49.7 MB (45.21%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,24 +1,19 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> map = new HashMap<>();
+    public List<List<String>> groupAnagrams(String[] strs) {Map<String, List<String>> map = new HashMap<>();
         String[] str = new String[strs.length];
-        for(int a =0; a<strs.length; a++){
-            char[] chars = strs[a].toCharArray();
+        for(int i=0; i<strs.length; i++){
+            char[] chars = strs[i].toCharArray();
             Arrays.sort(chars);
-            String sorted = new String(chars);
-            str[a]= sorted;
-
+            str[i]=Arrays.toString(chars);
         }
-        for(int i =0; i<strs.length; i++){
+        List<List<String>> listKQ= new ArrayList<>();
+        for(int i=0; i<strs.length; i++){
             List<String> list = map.getOrDefault(str[i], new ArrayList<>());
             list.add(strs[i]);
-            map.put(str[i], list);
-
+            map.put(str[i],list);
         }
-        List<List<String>> list = new ArrayList<>();
-        list.addAll(map.values());
-        return list;
-        
+        listKQ.addAll(map.values());
+        return listKQ;
         
     }
 }
